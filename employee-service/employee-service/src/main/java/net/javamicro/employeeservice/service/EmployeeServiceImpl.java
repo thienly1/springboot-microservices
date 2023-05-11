2aec16afebb363f44d36381340a16a0b8b54717a
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-11 15:08:20 +0200

    Using WebClient to communicate between microservices 1. Add Spring WebFlux Dependency: because WebClient is presented in the spring 2. Configure WebClient as Spring Bean 3. Inject and use WebClient to call the Rest API 4. Test using Postman Client

--- employee-service/employee-service/src/main/java/net/javamicro/employeeservice/service/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/net/javamicro/employeeservice/service/EmployeeServiceImpl.java
@@ -9,13 +9,15 @@ import net.javamicro.employeeservice.repository.EmployeeRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+    //private RestTemplate restTemplate;
+    private WebClient webClient;
     @Override
     public EmployeeDto savedEmployee(EmployeeDto employeeDto) {
         Employee employee = new Employee(
@@ -39,12 +41,17 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee= employeeRepository.findById(employeeId).get();
+/*
         ResponseEntity<DepartmentDto> responseEntity= restTemplate.getForEntity(
                 "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
                 DepartmentDto.class
-        );
-        DepartmentDto departmentDto = responseEntity.getBody();
-
+                DepartmentDto departmentDto = responseEntity.getBody();
+*/
+        DepartmentDto departmentDto= webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
                 employee.getFirstName(),
