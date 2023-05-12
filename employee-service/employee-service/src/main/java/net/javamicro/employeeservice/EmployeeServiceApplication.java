d68442638b1b9c43097eed21b649e1a59242df8b
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-12 10:34:53 +0200

    - CREATE and Set up Service-Registry Spring boot project in Intellij -Register DepartmentService Microservice as Eureka Client -Register EmployeeService Microservice as Eureka Client

--- employee-service/employee-service/src/main/java/net/javamicro/employeeservice/EmployeeServiceApplication.java
+++ employee-service/employee-service/src/main/java/net/javamicro/employeeservice/EmployeeServiceApplication.java
@@ -3,6 +3,7 @@ package net.javamicro.employeeservice;
 import net.javamicro.employeeservice.dto.EmployeeDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 /*	@Bean
 	public RestTemplate restTemplate(){
