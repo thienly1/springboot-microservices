9f727597a9eaec9dbd763f63890bdeb596417da1
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-17 09:32:04 +0200

    ReFresh Use Case - create MessageController.java class set up the spring.boot.message in department-service.properties - Check with postman - set up to enable actuator - changes the message in department-service.properties - rerun ConfigServer - trigger refresh actuator on postman with postmapping: localhost:8080/actuator/refresh - Check message again with getmapping on postman. http://localhost:8080/message

---
+++ department-service/department-service/src/main/java/net/javamicro/departmentservice/controller/MessageController.java
@@ -0,0 +1,19 @@
+package net.javamicro.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope    // this annotation will force this spring bean to reload the configuration, it will trigger the refresh event to reload the configuration for this MessageController Spring bean
+public class MessageController {
+
+    @Value("${spring.boot.message}")  //we use this annotation to retrieve a value for this property from the configuration
+    private String message;
+
+    @GetMapping("message")
+    public String message(){
+        return message;
+    }
+}
