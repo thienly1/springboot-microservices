c3b2d88f5bee2532c3330b3977799b4e15a3a2c2
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-19 14:17:56 +0200

    Retry Pattern Implementation 1. Using @Retry annotation to a method (it is calling to external service) 2. Fallback method implementation 3. Add Retry configuration in application.properties file 4. Restart employee-service and demo - Restart employee-service application - Stop department-service to test how retry pattern work - make a call on getEmployeeById API by web browser or postman.you will see the default department along with this employee    http://localhost:9191/api/employees/2 - Check on a console log of employee-service, you will see it retried to call 5 times, the waiting time for each recall is 1s then after that, it forwarded to fallback method and return defaultDepartment

--- employee-service/employee-service/src/main/java/net/javamicro/employeeservice/service/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/net/javamicro/employeeservice/service/EmployeeServiceImpl.java
@@ -1,12 +1,15 @@
 package net.javamicro.employeeservice.service;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import net.javamicro.employeeservice.dto.APIResponseDto;
 import net.javamicro.employeeservice.dto.DepartmentDto;
 import net.javamicro.employeeservice.dto.EmployeeDto;
 import net.javamicro.employeeservice.entity.Employee;
 import net.javamicro.employeeservice.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private EmployeeRepository employeeRepository;
     //private RestTemplate restTemplate;
     private WebClient webClient;
@@ -40,9 +44,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return savedEmployeeDto;
     }
 
+
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
-    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     public APIResponseDto getEmployeeById(Long employeeId) {
+        LOGGER.info("inside getEmployeeById() method");
         Employee employee= employeeRepository.findById(employeeId).get();
 /*
         ResponseEntity<DepartmentDto> responseEntity= restTemplate.getForEntity(
@@ -73,6 +80,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     //implement a fallback method, it should have the same return type as the method using this fallback method and the name is same with fallbackmethod
     public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception){
+        LOGGER.info("inside getEmployeeById() method");
         Employee employee= employeeRepository.findById(employeeId).get();
 
         //create one default department as you want
