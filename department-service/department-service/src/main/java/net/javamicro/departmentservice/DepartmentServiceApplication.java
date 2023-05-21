45dc9dd3636886a530f0b3b36944fdfeadeb464b
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-21 05:24:36 +0200

    Generate REST API Documentation for Department Service using SpringDoc Open API

--- department-service/department-service/src/main/java/net/javamicro/departmentservice/DepartmentServiceApplication.java
+++ department-service/department-service/src/main/java/net/javamicro/departmentservice/DepartmentServiceApplication.java
@@ -1,11 +1,27 @@
 package net.javamicro.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
+@OpenAPIDefinition(info = @Info(title = "Department-Service REST APIS",
+                                description = "Department Service REST APIs documentation",
+		                        version = "v1.0",
+		                        contact = @Contact(name = "LyTa", email = "ly@mail.com", url = "https://www.lyta.com"),
+		                        license = @License(name = "Apache 2.9", url = "https://lyta.com")
+                                ),
+                   externalDocs = @ExternalDocumentation(
+						   description = "Department-service Docs",
+						   url = "https://lyta.com"
+				   )
+)
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
--- department-service/department-service/src/main/java/net/javamicro/departmentservice/controller/DepartmentController.java
+++ department-service/department-service/src/main/java/net/javamicro/departmentservice/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package net.javamicro.departmentservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.javamicro.departmentservice.dto.DepartmentDto;
 import net.javamicro.departmentservice.service.DepartmentService;
@@ -7,6 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Department Service - DepartmentController",
+        description = "Department Service - DepartmentController Exposes APIs for Department Service"
+)
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/departments")
@@ -15,11 +22,28 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     //Build save department REST API
+    @Operation(
+            summary = "Save Department REST API",
+            description = "Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public  ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto savedDepartmentDto= departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(savedDepartmentDto, HttpStatus.CREATED);
     }
+
+    @Operation(
+            summary = "Get Department REST API",
+            description = "Get Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
         DepartmentDto departmentByCode = departmentService.getDepartmentByCode(departmentCode);
